1006a10b7928d8a9888733658eed0cd705947265
Austin Walker <Austin Walker>
2016-09-13 16:51:21 -0600

    Chatty Done

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,15 +1,22 @@
-'use strict';
+// 'use strict';
 
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
+    $scope.test = function () {
+
+
     messageService.getMessages().then(function ( response ) {
       $scope.messages = response.data;
     });
+  };
+    $scope.test();
 
-    $scope.addMessage = function ( message ) {
+    $scope.addMessage = function (message) {
+
       if (message) {
         messageService.addMessage(message).then(function ( response ) {
           $scope.messages = response.data;
+          $scope.newMessage = "";
         });
       }
     };
